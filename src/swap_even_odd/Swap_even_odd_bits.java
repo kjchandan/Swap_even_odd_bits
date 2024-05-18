40e54bd03b6820c210ba2dacae079e1ca0e8a3e1
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-18 21:28:40 +0530

    all chnages done

--- src/swap_even_odd/Swap_even_odd_bits.java
+++ src/swap_even_odd/Swap_even_odd_bits.java
@@ -9,8 +9,14 @@ public class Swap_even_odd_bits {
 		
 		//Swapping even and odd bits 
 		//(0xAAAAAAAA => 14316555765)  //All odd position are 1 and even are 1.
+									   //& opearation=> later after & operation all odd are preserved and even bits are 0
+									   //<<1=>later all odd occupies even positions
 		//(0X55555555 => -1431655766)  //All even position are 1 and odd are 0.
+										//& operation => later after & operation all even bts are preserved same and odd bits will become xeros.
+										//>>>1=>All even posotions occupies odd positions.
 
+
+										//or(|)=>Add both the above. Will interchange even and odd positions.
 		System.out.println((n & 0xAAAAAAAA)>>>1 | (n & 0x55555555)<<1);  
 		
 		//122				
