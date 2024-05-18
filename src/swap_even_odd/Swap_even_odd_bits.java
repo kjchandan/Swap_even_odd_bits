ef03de234cdf501bc871b81c1129973ff43ed183
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-18 21:24:55 +0530

    commit

--- src/swap_even_odd/Swap_even_odd_bits.java
+++ src/swap_even_odd/Swap_even_odd_bits.java
@@ -8,8 +8,8 @@ public class Swap_even_odd_bits {
 		int n = scan.nextInt(); //181
 		
 		//Swapping even and odd bits 
-		//(0xAAAAAAAA => 14316555765) 
-		//(0X55555555 => -1431655766)
+		//(0xAAAAAAAA => 14316555765)  //All odd position are 1 and even are 1.
+		//(0X55555555 => -1431655766)  //All even position are 1 and odd are 0.
 
 		System.out.println((n & 0xAAAAAAAA)>>>1 | (n & 0x55555555)<<1);  
 		
